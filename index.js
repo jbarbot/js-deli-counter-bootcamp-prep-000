f350868f2c4e7936bd4cea95a51d76bb5cb00676
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 16:57:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line) {
   
   const lineOrder = []
   for (let i = 0, l = line.length; i < l; i++) {
-    lineOrder.push(`${i+1}. ${line[i]}.`)
+    lineOrder.push(`The line is currently: ${i+1}. ${line[i]}.`)
   }
   }
 }
