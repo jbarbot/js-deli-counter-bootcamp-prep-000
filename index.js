24201f9066fff03aed16f56d571ffe5f07cffa6e
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 16:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ function nowServing(line) {
 function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
-  } else {
+  }
   
   const lineOrder = []
   for (let i = 0, l = line.length; i < l; i++) {
     lineOrder.push(`The line is currently: ${i+1}. ${line[i]}.`)
-  }
   }
 }
