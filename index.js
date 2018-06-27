20feb05a71c2610d1a64ecfea5fffbc5fbd03334
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 16:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = []
 
 function takeANumber(line, name) {
   line.push(name)
-  return `Welcome, ${name}. You are ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
