7127e6a69ddb9042765554cafa1a6cdb11bd3645
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 16:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing 
+function nowServing(line) {
+  if (line.length = 0) {
+  return "There is nobody waiting to be served!"
+  } else {
+    return katzDeli[1]
+  }
+}
